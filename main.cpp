a1affe7f0d92993ec5f26291efd177d2252afa29
sprax <sprax@comcast.net>
2019-09-03 17:56:04 -0400

    ready

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 int main(int argc, char **argv)
 {
+    std::cerr << "Ready 4 Rev" << std::endl;
     std::cerr << "Hello World" << std::endl;
     std::cout << "Howdy Earth" << std::endl;
     std::cout << "What up WIP" << std::endl;
