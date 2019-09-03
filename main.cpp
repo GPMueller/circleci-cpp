c8d24cd9f77eb4cdd2f34a5cea4308c04a807dd1
sprax <sprax@comcast.net>
2019-09-03 17:53:04 -0400

    whatup

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main(int argc, char **argv)
 {
     std::cerr << "Hello World" << std::endl;
     std::cout << "Howdy Earth" << std::endl;
+    std::cout << "What up WIP" << std::endl;
     std::cout << "Zdrasti Orb" << std::endl;
 }
